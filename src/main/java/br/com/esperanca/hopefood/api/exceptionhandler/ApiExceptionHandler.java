dd0abf7178bd3f9418cd5a4015d4b70086e505b5
Leonardo Esperança <dev.esperanca.leonardo@gmail.com>
2023-01-27 15:31:27 -0300

    :recycle: Expressão lambda.
    Foi criado uma expressão lambda para melhor legibilidade do código.

--- src/main/java/br/com/esperanca/hopefood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/esperanca/hopefood/api/exceptionhandler/ApiExceptionHandler.java
@@ -8,13 +8,17 @@ import com.fasterxml.jackson.databind.JsonMappingException.Reference;
 import com.fasterxml.jackson.databind.exc.IgnoredPropertyException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,10 +30,14 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
+@AllArgsConstructor
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+  private MessageSource messageSource;
 
   private static final String MENSAGEM_GENERICA = "Ocorreu um erro interno " +
     "no sistema. Tente novamente e se o problema persistir, entre em " +
@@ -115,13 +123,17 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     BindingResult result = exception.getBindingResult();
 
+    Function<FieldError, Campo> camposInvalidos = fieldError ->
+      Campo.builder()
+        .nome(fieldError.getField())
+        .mensagem(messageSource.getMessage(fieldError,
+          LocaleContextHolder.getLocale()))
+      .build();
+
     List<Campo> campos = result.getFieldErrors()
       .stream()
-      .map(fieldError -> Campo.builder()
-          .nome(fieldError.getField())
-          .mensagem(fieldError.getDefaultMessage())
-        .build()
-      ).collect(Collectors.toList());
+      .map(camposInvalidos)
+      .collect(Collectors.toList());
 
     var erro = criarErroBuilder(tipoErro, status, mensagem)
       .campos(campos)
