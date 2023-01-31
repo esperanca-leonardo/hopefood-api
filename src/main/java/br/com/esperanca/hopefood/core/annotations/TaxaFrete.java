44912371e63e227fc4369b2ede5541c91c25bb35
Leonardo Esperança <dev.esperanca.leonardo@gmail.com>
2023-01-31 15:52:36 -0300

    :coffin: Otimização dos imports.

--- src/main/java/br/com/esperanca/hopefood/core/annotations/TaxaFrete.java
+++ src/main/java/br/com/esperanca/hopefood/core/annotations/TaxaFrete.java
@@ -1,26 +1,23 @@
 package br.com.esperanca.hopefood.core.annotations;
 
 import javax.validation.Constraint;
-import javax.validation.OverridesAttribute;
 import javax.validation.Payload;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.*;
 
 @NotNull
 @PositiveOrZero
 @Constraint(validatedBy = {})
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE,
-  ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE}
-)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 public @interface TaxaFrete {
 
-  String message() default "O valor informado deve ser maior ou igual a zero";
+  String message() default "";
 
   Class<?>[] groups() default {};
 
