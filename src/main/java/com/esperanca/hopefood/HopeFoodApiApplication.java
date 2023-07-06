a6ec78e11eb7923d218803eb6dd29992297285e0
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-07-06 16:30:06 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/esperanca/hopefood/HopeFoodApiApplication.java
@@ -0,0 +1,12 @@
+package com.esperanca.hopefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeFoodApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/esperanca/hopefood/HopeFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esperanca.hopefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
