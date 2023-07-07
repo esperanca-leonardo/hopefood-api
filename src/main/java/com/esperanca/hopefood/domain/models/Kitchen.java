07f7894ea572418d74d0058d39c488ceca5f7405
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-07-07 16:48:03 -0300

    :construction: Adicionar classes Kitchen e Restaurant

--- src/main/java/com/esperanca/hopefood/domain/models/Kitchen.java
+++ src/main/java/com/esperanca/hopefood/domain/models/Kitchen.java
@@ -1,2 +1,18 @@
-package com.esperanca.hopefood.domain.models;public class Kitchen {
+package com.esperanca.hopefood.domain.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Kitchen {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String description;
 }
--- src/main/java/com/esperanca/hopefood/domain/models/Restaurant.java
+++ src/main/java/com/esperanca/hopefood/domain/models/Restaurant.java
@@ -1,2 +1,19 @@
-package com.esperanca.hopefood.domain.models;public class Restaurant {
+package com.esperanca.hopefood.domain.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Restaurant {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private BigDecimal freightValue;
 }
