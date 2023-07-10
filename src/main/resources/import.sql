4fc97f375e2a7cfea356a1c3d09a5fdbc920758a
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-07-10 12:31:23 -0300

    :wrench: Adicionados inserts no arquivo import.sql para inserir dados nas seguintes tabelas: state, city, form_of_payment e permission

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,15 @@ INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("Ristorante del
 INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("Pizzaria Bella Napoli", 1, 4.99);
 INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("Taco Loco", 2, 6.50);
 INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("El Sabor Auténtico", 2, 7.99);
+
+INSERT INTO state (name) VALUES ('São Paulo');
+INSERT INTO state (name) VALUES ('Rio de Janeiro');
+
+INSERT INTO city (name, state_id) VALUES ('Assis', 1);
+INSERT INTO city (name, state_id) VALUES ('Angra dos Reis', 2);
+
+INSERT INTO form_of_payment (description) VALUES ('Cartão de Crédito');
+INSERT INTO form_of_payment (description) VALUES ('Boleto Bancário');
+
+INSERT INTO permission (name, description) VALUES ('Admin', 'Permissão de Administrador');
+INSERT INTO permission (name, description) VALUES ('Usuário', 'Permissão de Usuário');
