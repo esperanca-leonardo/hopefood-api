c6d4ba2c032ad6a6820895546febfb4e1b247332
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-07-10 10:59:01 -0300

    :wrench: Adicionar 4 imports de restaurant

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
 INSERT INTO kitchen (name, description) VALUES ("Italiana", "Massas, pizzas e molhos saborosos. Ingredientes frescos como azeite de oliva, tomates, queijos e ervas aromáticas. Tradição e autenticidade em cada prato.");
 INSERT INTO kitchen (name, description) VALUES ("Mexicana", "Sabores picantes e vibrantes. Pimentas, feijões, milho, abacate, tomates e especiarias. Tacos, burritos, guacamole e enchiladas. Riqueza cultural em cada mordida.");
+
+INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("Ristorante delizioso", 1, 5.99);
+INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("Pizzaria Bella Napoli", 1, 4.99);
+INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("Taco Loco", 2, 6.50);
+INSERT INTO restaurant (name, kitchen_id, freight_value) VALUES ("El Sabor Auténtico", 2, 7.99);
