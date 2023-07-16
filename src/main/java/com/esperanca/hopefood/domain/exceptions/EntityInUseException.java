c701f4d20e1bde51c9cdee5854e6a01e1fe7d66d
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-07-16 12:55:33 -0300

    :recycle: Basic exceptions

--- src/main/java/com/esperanca/hopefood/domain/exceptions/EntityInUseException.java
+++ src/main/java/com/esperanca/hopefood/domain/exceptions/EntityInUseException.java
@@ -1,6 +1,11 @@
 package com.esperanca.hopefood.domain.exceptions;
 
-public class EntityInUseException extends RuntimeException {
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+@ResponseStatus(CONFLICT)
+public class EntityInUseException extends BusinessLogicException {
 
 	public EntityInUseException(String message) {
 		super(message);
--- src/main/java/com/esperanca/hopefood/domain/exceptions/EntityNotFoundException.java
+++ src/main/java/com/esperanca/hopefood/domain/exceptions/EntityNotFoundException.java
@@ -1,6 +1,11 @@
 package com.esperanca.hopefood.domain.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ResponseStatus(NOT_FOUND)
+public class EntityNotFoundException extends BusinessLogicException {
 
 	public EntityNotFoundException(String message) {
 		super(message);
