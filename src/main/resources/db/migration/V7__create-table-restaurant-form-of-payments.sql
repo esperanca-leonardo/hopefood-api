9304ea0cb84bdb7219c1e10ea8de64850c485ce2
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-08-04 13:22:06 -0300

    :recycle: Alterado o nome da coluna form_of_payment para form_of_payments

--- src/main/resources/db/migration/V7__create-table-restaurant-form-of-payments.sql
+++ src/main/resources/db/migration/V7__create-table-restaurant-form-of-payments.sql
@@ -1,7 +1,8 @@
 CREATE TABLE restaurant_form_of_payments (
   restaurant_id BIGINT NOT NULL,
-  form_of_payment_id BIGINT NOT NULL,
+  form_of_payments_id BIGINT NOT NULL,
 
   CONSTRAINT fk_restaurant_form_of_payment_restaurant FOREIGN KEY (restaurant_id) REFERENCES restaurant (id),
-  CONSTRAINT fk_restaurant_form_of_payment_form_of_payment_id FOREIGN KEY (form_of_payment_id) REFERENCES form_of_payment (id)
+  CONSTRAINT fk_restaurant_form_of_payment_form_of_payment_id FOREIGN KEY (form_of_payments_id)
+  REFERENCES form_of_payment (id)
 ) engine=InnoDB default charset=utf8;
