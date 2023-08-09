e0bc4de8eac17c4fb9b6cf26e282412872701622
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2023-08-09 18:03:32 -0300

    :construction: Adicionado enums para mensagens de erro de cidade: CITY_NOT_FOUND e CITY_IN_USE

--- src/main/java/com/esperanca/hopefood/api/errors/ErrorType.java
+++ src/main/java/com/esperanca/hopefood/api/errors/ErrorType.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorType {
+	INVALID_DATA("/invalid-data", "Invalid data"),
 	BUSINESS_LOGIC("/business-logic", "Business Logic"),
 	ENTITY_IN_USE("/entity-in-use", "Entity in use"),
 	ENTITY_NOT_FOUND("/entity-not-found", "Entity not found"),
@@ -12,7 +13,8 @@ public enum ErrorType {
 	RESTAURANT_NOT_FOUND("/restaurant-not-found", "Restaurant not found"),
 	STATE_IN_USE("/state-in-use", "State in use"),
 	STATE_NOT_FOUND("/state-not-found", "State not found"),
-	INVALID_DATA("/invalid-data", "Invalid data");
+	CITY_NOT_FOUND("/city-not-found", "City not found"),
+	CITY_IN_USE("/city-in-use", "City in use");
 
 	private final String title;
 	private final String path;
